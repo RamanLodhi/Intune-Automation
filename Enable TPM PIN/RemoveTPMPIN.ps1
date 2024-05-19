0ea7d16900ce476f596dd3b70d1863d140033d8c
raman <ramanlodhiddun@gmail.com>
2024-05-19 20:15:37 +0530

    Updated with detection method

--- Enable TPM PIN/RemoveTPMPIN.ps1
+++
@@ -1,3 +0,0 @@
-$BLV = Get-BitlockerVolume -MountPoint "C:"
-$TpmPinKeyProtector = $BLV.KeyProtector | Where-Object {$PSItem.KeyProtectorType -eq "TpmPin"}
-Remove-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $TpmPinKeyProtector.KeyProtectorId
